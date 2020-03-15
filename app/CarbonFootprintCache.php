0060780f089b67a844ad9696afbe4349fb258dc8
Kaustubh Jadhav <jadhav.kaustubh@gmail.com>
2020-03-15 19:47:35 +0530

    adds global scope to only fetch cache within 24 hours

--- app/CarbonFootprintCache.php
+++ app/CarbonFootprintCache.php
@@ -2,9 +2,20 @@
 
 namespace App;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
 class CarbonFootprintCache extends Model
 {
     protected $guarded = ['id', 'created_at', 'updated_at'];
+
+
+    protected static function booted()
+    {
+        static::addGlobalScope('fresh', function (Builder $builder) {
+            $builder->where('created_at', '>',Carbon::yesterday());
+        });
+    }
+
 }
