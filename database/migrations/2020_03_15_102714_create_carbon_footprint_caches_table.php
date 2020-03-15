088383f9fc1f3f5d568cd64a698818826830efc9
Kaustubh Jadhav <jadhav.kaustubh@gmail.com>
2020-03-15 16:07:39 +0530

    adds migration for CarbonFootprintCache table

---
+++ database/migrations/2020_03_15_102714_create_carbon_footprint_caches_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCarbonFootprintCachesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('carbon_footprint_caches', function (Blueprint $table) {
+            $table->id();
+            $table->float('activity', 8, 2);
+            $table->char('activity_type', 10);
+            $table->char('fuel_type', 30)->nullable();
+            $table->char('mode', 30)->nullable();
+            $table->char('country', 10);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('carbon_footprint_caches');
+    }
+}
